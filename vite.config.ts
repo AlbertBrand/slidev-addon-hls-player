94dd98ee64f36c61dff5b8b728a017e5deb289b2
Albert Brand <abrand@xebia.com>
2025-06-24 12:04:17 +0200

    Fix optimizeDeps path

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   optimizeDeps: {
-    include: ["slidev-addon-tldraw > hls.js"],
+    include: ["slidev-addon-hls-player > hls.js"],
   },
 });
